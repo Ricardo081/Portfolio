315b8daded98d593b61083972043c7c78c70108a
Ricardo081 <ricardo.cardenas.personal@gmail.com>
2022-10-04 20:17:53 -0500

    update

--- monocles_(JS)/client/App.js
+++ monocles_(JS)/client/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Top_bar from './components/top_bar/Top_bar';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-    {/* <Top_bar/> */}
+    <Top_bar/>
     {/* <Birds/> */}
       <Text>Hi9</Text>
 
